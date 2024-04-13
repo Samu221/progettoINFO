bafdc92234a949d685562acb8bcf56776441d57c
CARTACCI DAVIDE <cartacci.davide@dominio.local>
2024-04-13 10:42:14 +0200

    sistemato il css

--- account.php
+++ account.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestione Account</title>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
 </head>
 <body>
     <header>
--- home.php
+++ home.php
@@ -16,18 +16,15 @@
                 <img src="immagini/logo.png" alt="Logo" class="logo">
             </a>
             <h1 class="h1"> DIYhub </h1>
-            <div class="search-bar">
-                <input type="text" id="search-input" placeholder="Cerca">
-            </div>
         </div>
         <a href="account.php" class="account-link">
             <img src="immagini/account.png" alt="Icona Account" style="width: 40px;">
         </a>
         <a href="/percorso-del-tuo-account" class="salvati-link">
-            <img src="immagini/salvati.png" alt="Icona Salvati" style="width: 35px;">
+            <img src="immagini/salvati.png" alt="Icona Salvati" style="width: 40px;">
         </a>
         <a href="inserimento.html" class="aggiungi-link">
-            <img src="immagini/aggiungi.png" alt="Icona Aggiungi" style="width: 34px;">
+            <img src="immagini/aggiungi.png" alt="Icona Aggiungi" style="width: 40px;">
         </a>
     </header>
 
@@ -45,7 +42,7 @@
     }
 
     // Execute the SQL query to retrieve all projects and related information
-    $sql = "SELECT progetto.CODICE, progetto.titolo, progetto.ambito, progetto.descrizione, progetto.num_like, immagine.immagine, progetto.username_creatore
+    $sql = "SELECT progetto.CODICE, progetto.titolo, progetto.ambito, progetto.descrizione, immagine.immagine, progetto.username_creatore
             FROM progetto
             INNER JOIN account ON progetto.username_creatore = account.username
             INNER JOIN immagine ON progetto.CODICE = immagine.codice_progetto
@@ -63,8 +60,6 @@
             echo "  </div>";
             echo "  <div class='container info'>";
             echo "      <h2 class='titolo'>" . $row["titolo"] . "</h2>";
-            echo "      <div class='container likes'>like da inserire"; 
-            echo "      </div>";
             echo "      <div class='container ambito'>";
             echo "          <p>Creato da: " . $row["username_creatore"] . "</p>";
             echo "          <p>Ambito: " . $row["ambito"] . "</p>";
